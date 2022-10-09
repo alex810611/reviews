57ada312f70f8a04233fc95ef692276eb325f3dc
alex810611 <alex810611@gmail.com>
2022-10-09 14:20:42 +0800

    calcuate

--- reviews.py
+++ reviews.py
@@ -1,10 +1,16 @@
-date = []
+data = []
 count = 0
 with open ('reviews.txt', 'r') as f :
 	for line in f :
-		date.append(line)
+		data.append(line)
 		count = count + 1
 		if count % 10000 == 0 : # % 求餘數
-			print(len(date))
-print(date[0])
+			print(len(data))
+print(data[0])
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+	
+print ('每筆留言平均長度為', sum_len/len(data))
 
