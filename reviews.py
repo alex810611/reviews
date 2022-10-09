7c71b72a649b82b674384e3955b1efb8f91000e2
alex810611 <alex810611@gmail.com>
2022-10-09 14:55:26 +0800

    filter

--- reviews.py
+++ reviews.py
@@ -10,7 +10,15 @@ print(data[0])
 
 sum_len = 0
 for d in data:
-	sum_len = sum_len + len(d)
+	sum_len = sum_len + len(d) #計算總留言文字數量
 	
 print ('每筆留言平均長度為', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100 :
+		new.append(d)
+print('一共有', len(new), '小於100個字的留言')
+print(new[0])
+print(new[1])
 
